2c0d45f41e82129f113c10e404f6ef3130119bac
Francisco Muñoz <femunoz@dim.uchile.cl>
2022-09-29 00:27:30 -0300

    feat: agregar métodos totalContent y emptyContent

--- src/drinks/Glass.java
+++ src/drinks/Glass.java
@@ -1,20 +1,21 @@
 package drinks;
 
 import ingredients.interfaces.Ingredient;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Representa un vaso con ingredientes.
  */
 public class Glass {
-  private List<Ingredient> ingredients;
+  private ArrayList<Ingredient> ingredients;
 
   /**
    * Constructor del vaso.
    *
    * @param ingredients Lista de ingredientes.
    */
-  public Glass(List<Ingredient> ingredients) {
+  public Glass(ArrayList<Ingredient> ingredients) {
     this.ingredients = ingredients;
   }
 
@@ -24,7 +25,17 @@ public class Glass {
   }
 
   /** Cambia la lista de ingredientes. */
-  public void setIngredients(List<Ingredient> ingredients) {
+  protected void setIngredients(ArrayList<Ingredient> ingredients) {
     this.ingredients = ingredients;
+  }
+
+  /** Retorna la cantidad de contenido en el vaso total en mililitros. */
+  public Integer totalContent() {
+    return 0;
+  }
+
+  /** Vacia el contenido del vaso. */
+  public void emptyContent() {
+    // Hacer algo uwu
   }
 }
